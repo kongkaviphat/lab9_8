b12b779517985e18b6b494aeb6c4fe1ce30c78f4
kongkaviphat <kongkaviphats@gmail.com>
2022-10-08 22:09:52 +0700

    lab9_8
    นายกวิภัฎ แสนสม 6304062636022 sec 2

---
+++ workshop8.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert_wk8.php" method="post">
+        username:<input type="text" name="username"><br>
+        password:<input type="password" name="password"><br>
+        ชื่อ-นามสกุล:<input type="text" name="name"><br>
+        ที่อยู่:<br>
+        <textarea name="address" row="3" cols="40"></textarea><br>
+        เบอร์โทรศัพท์:<input type="text" name="mobile"><br>
+        อีเมล:<input type="text" name="email"><br>
+        <input type="submit" value="เพิ่มข้อมูล">
+    </form>
+</body>
+</html>
